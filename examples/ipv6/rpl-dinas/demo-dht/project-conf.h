d1fcef300f6fe15280de100bf073ee82545de444
bobib22 <bob.b22@gmx.fr>
2016-03-14 09:31:36 +0100

    modify the project conf to support multihop topologies - decrease Rx threshold - decrease Tx power

--- examples/ipv6/rpl-dinas/demo-dht/project-conf.h
+++ examples/ipv6/rpl-dinas/demo-dht/project-conf.h
@@ -28,31 +28,76 @@
 
 /**
  * \file
- *         Configuration file for demo-dht
+ *         Configuration file for demo4
  * \author
  *         Michele Amoretti <michele.amoretti@unipr.it> 
  */
 
+#define DEBUG_DINAS 0 //additional debug for dinas
+
+// #define RPL_CONF_LEAF_ONLY 1
 #define UIP_CONF_TCP 0
 #define PROCESS_CONF_NO_PROCESS_NAMES 1
 
 #define MAX_NUM_MSG 21
 #define NOTIFICATION_COUNTER 10 /* the pattern is N R R R R .. */
+// #define NUM_ROOMS 3  /* N = number of nodes */
 #define NUM_ROOMS 20  /* N = number of nodes */
-#define TTL 31 /* this is D in RPL-DHT, it is the max TTL allowed by DINASMSG */
-#define PERIOD 120
+#define TTL 4 /* this is D */
+// #define PERIOD 120
+// #define PERIOD 60
+// #define PERIOD 30
+#define PERIOD 10
 #define RANDWAIT (PERIOD)
+// #define RANDWAIT 10
+
+//https://github.com/iot-lab/openlab/blob/master/net/phy_rf2xx/phy_rf2xx.c#L994
+#define RF2XX_TX_POWER  PHY_POWER_m17dBm //(min)
+// #define RF2XX_TX_POWER  PHY_POWER_m12dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m9dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m7dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m5dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m4dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m3dBm
+// #define RF2XX_TX_POWER  PHY_POWER_m2dBm
+// #define RF2XX_TX_POWER  PHY_POWER_0dBm   // default
+// #define RF2XX_TX_POWER  PHY_POWER_3dBm   // max
+
+// #define RF2XX_RX_RSSI_THRESHOLD  RF2XX_PHY_RX_THRESHOLD__m101dBm //(default value)
+// #define RF2XX_RX_RSSI_THRESHOLD  RF2XX_PHY_RX_THRESHOLD__m90dBm  
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m87dBm   
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m84dBm   
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m81dBm   
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m78dBm   
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m75dBm   
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m72dBm   
+#define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m69dBm
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m66dBm
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m63dBm
+// #define RF2XX_RX_RSSI_THRESHOLD  RF2XX_PHY_RX_THRESHOLD__m60dBm  //too weak for TxmA7dBm
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m57dBm   //too weak for TxmA7dBm
+// #define RF2XX_RX_RSSI_THRESHOLD RF2XX_PHY_RX_THRESHOLD__m54dBm   //too weak for TxmA7dBm
+// #define RF2XX_RX_RSSI_THRESHOLD  RF2XX_PHY_RX_THRESHOLD__m51dBm  //too weak for TxmA7dBm
+// #define RF2XX_RX_RSSI_THRESHOLD  RF2XX_PHY_RX_THRESHOLD__m48dBm  //too weak for TxmA7dBm
+
+// #define RF2XX_CHANNEL 11 // entre 11 et 26 sinon par défaut 22 (contiki/platform/openlab/radio-rf2xx.c)
+// #define RF2XX_CHANNEL 18 // entre 11 et 26 sinon par défaut 22
+#define RF2XX_CHANNEL 26 // entre 11 et 26 sinon par défaut 22
 
 /* overwrite params defined in bloom.h */
 #define BLOOM_SIZE_PARAM 40
 #define BLOOM_NFUNCS_PARAM 7
 
 /* overwrite params defined in proximity-cache.h */
-#define CACHE_SIZE_PARAM 20 /* this is C */
-#define T1_PARAM 0
+// #define CACHE_SIZE_PARAM 11 /* this is C */
+#define CACHE_SIZE_PARAM 11 /* this is C */
+
+#define T1_PARAM 30
+// #define T1_PARAM 0
 #define T2_PARAM 90
 
-#define P_FAIL 20
+/* overwrite params defined in dinas-updown.h */
+#define FLOODING_PARAM 0
 
-
+#define P_FAIL 0
 
